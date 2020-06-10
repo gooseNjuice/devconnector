b77b7e415c450249a4edefd77656fee34bf2e367
GooseNJuice <zamirkononov@gmail.com>
2020-06-10 11:52:34 +0300

    routes test

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
-const app=express();
+const app = express();
 
+// Connect Database
 connectDB();
 
-const PORT=process.env.PORT || 5000;
+app.get('/', (req, res) => res.send('API running'));
 
-app.get('/', (req,res)=>res.send('API running'));
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-app.listen(PORT, ()=>console.log(`The server is listening on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`The server is listening on port ${PORT}`));
