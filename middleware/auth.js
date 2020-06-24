2640c2b99987ab1016626dc039dd030971370521
GooseNJuice <zamirkononov@gmail.com>
2020-06-24 10:53:03 +0300

    auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+    //get token from header
+    const token = req.header('x-auth-token')
+    //check if no token
+    if (!token) {
+        return res.status(401).json({msg: 'No token, authorization denied'})
+    }
+    //verify token
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded.user;
+        next();
+    } catch (e) {
+res.status(401).json({msg: 'Token is not valid'})
+    }
+}
--- routes/api/users.js
+++ routes/api/users.js
@@ -47,7 +47,7 @@ router.post('/', [
 
             const payload={
                 user:{
-                    id:user.id
+                    id:user.id,
                 }
             }
             jwt.sign(payload, config.get('jwtSecret'),
