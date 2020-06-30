bfad88b1a6ed649f8fb4819dc17ff5c765167379
GooseNJuice <zamirkononov@gmail.com>
2020-06-30 13:42:42 +0300

    Display profiles

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import CreateProfile from "./components/profile-form/CreateProfile";
 import EditProfile from "./components/profile-form/EditProfile";
 import AddExperience from "./components/profile-form/AddExperience";
 import AddEducation from "./components/profile-form/AddEducation";
+import Profiles from "./components/profiles/Profiles";
 import Alert from "./components/layout/Alert";
 import NavBar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -35,6 +36,7 @@ const App = () => {
                         <Switch>
                             <Route exact path='/register' component={Register}/>
                             <Route exact path='/login' component={Login}/>
+                            <Route exact path='/profiles' component={Profiles}/>
                             <PrivateRoute exact path='/dashboard' component={Dashboard}/>
                             <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
                             <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
