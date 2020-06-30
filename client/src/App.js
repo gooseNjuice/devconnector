caaf02758b852fb90ed457a8f168264afa5db10e
GooseNJuice <zamirkononov@gmail.com>
2020-06-30 17:18:51 +0300

    Post action, reducer & initial component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
+import Posts from "./components/posts/Posts";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import EditProfile from "./components/profile-form/EditProfile";
 import AddExperience from "./components/profile-form/AddExperience";
@@ -44,6 +45,7 @@ const App = () => {
                             <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
                             <PrivateRoute exact path='/add-experience' component={AddExperience}/>
                             <PrivateRoute exact path='/add-education' component={AddEducation}/>
+                            <PrivateRoute exact path='/posts' component={Posts}/>
                         </Switch>
                     </section>
                 </Fragment>
