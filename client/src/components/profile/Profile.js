4d4534c6dbe46a8fb3045e8ebf4bed329a98babd
GooseNJuice <zamirkononov@gmail.com>
2020-06-30 15:12:23 +0300

    Profile Experience  Education Display

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,38 +5,72 @@ import {Link} from "react-router-dom";
 import {getProfileById} from "../../actions/profile";
 import Spinner from "../layout/Spinner";
 import ProfileAbout from "./ProfileAbout";
+import ProfileEducation from "./ProfileEducation";
+import ProfileExperience from "./ProfileExperience";
 import ProfileTop from "./ProfileTop";
 
 
 const Profile = ({getProfileById, profile: {profile, loading}, auth, match}) => {
-    useEffect(()=> {
+    useEffect(() => {
         getProfileById(match.params.id)
-    },[getProfileById, match.params.id])
-        return (
-            <Fragment>
-                {profile === null || loading? <Spinner/>: <Fragment>
-                    <Link to='/profiles' className="btn bg-light">
-                        Back To Profiles
-                    </Link>
-                    {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id
-                    && (<Link to='/edit/profile' className="btn btn-dark">Edit profile</Link>)}
-                    <div className="profile-grid my-1">
-<ProfileTop profile={profile} />
-<ProfileAbout profile={profile} />
+    }, [getProfileById, match.params.id])
+    return (
+        <Fragment>
+            {profile === null || loading ? <Spinner/> : <Fragment>
+                <Link to='/profiles' className="btn bg-light">
+                    Back To Profiles
+                </Link>
+                {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id
+                && (<Link to='/edit/profile' className="btn btn-dark">Edit profile</Link>)}
+                <div className="profile-grid my-1">
+                    <ProfileTop profile={profile}/>
+                    <ProfileAbout profile={profile}/>
+                    <div className='profile-exp bg-white p-2'>
+                        <h2 className='text-primary'>Experience</h2>
+                        {profile.experience.length > 0 ? (
+                            <Fragment>
+                                {profile.experience.map(experience => (
+                                    <ProfileExperience
+                                        key={experience._id}
+                                        experience={experience}
+                                    />
+                                ))}
+                            </Fragment>
+                        ) : (
+                            <h4>No experience credentials</h4>
+                        )}
                     </div>
-                </Fragment>}
-            </Fragment>)
-    }
+
+
+                    <div className='profile-edu bg-white p-2'>
+                        <h2 className='text-primary'>Education</h2>
+                        {profile.education.length > 0 ? (
+                            <Fragment>
+                                {profile.education.map(education => (
+                                    <ProfileEducation
+                                        key={education._id}
+                                        education={education}
+                                    />
+                                ))}
+                            </Fragment>
+                        ) : (
+                            <h4>No education credentials</h4>
+                        )}
+                    </div>
+
+                </div>
+            </Fragment>}
+        </Fragment>)
+}
 Profile.propTypes = {
     getProfileById: PropTypes.func.isRequired,
     profile: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired
 }
-const mapStateToProps= state => ({
+const mapStateToProps = state => ({
     profile: state.profile,
     auth: state.auth
 })
-
 
 
 export default connect(mapStateToProps, {getProfileById})(Profile)
