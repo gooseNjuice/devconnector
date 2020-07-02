761e07c95fd87de4adf6e900941b53cd913e197b
GooseNJuice <zamirkononov@gmail.com>
2020-07-02 17:22:27 +0300

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,39 @@
-import React, {Fragment, useEffect} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Dashboard from "./components/dashboard/Dashboard";
-import Post from "./components/post/Post";
-import Posts from "./components/posts/Posts";
-import CreateProfile from "./components/profile-form/CreateProfile";
-import EditProfile from "./components/profile-form/EditProfile";
-import AddExperience from "./components/profile-form/AddExperience";
-import AddEducation from "./components/profile-form/AddEducation";
-import Profile from "./components/profile/Profile";
-import Profiles from "./components/profiles/Profiles";
-import Alert from "./components/layout/Alert";
-import NavBar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import PrivateRoute from "./components/routhing/PrivateRoute";
-import setAuthToken from "./helpers/setAuthToken";
-import {Provider} from 'react-redux';
-import store from "./store";
-import {loadUser} from "./actions/auth";
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
 
 if (localStorage.token) {
-    setAuthToken(localStorage.token)
+  setAuthToken(localStorage.token);
 }
+
 const App = () => {
-    useEffect(() => {
-        store.dispatch(loadUser())
-    }, [])
-    return (
-        <Provider store={store}>
-            <Router>
-                <Fragment>
-                    <NavBar/>
-                    <Route exact path='/' component={Landing}/>
-                    <section className="container">
-                        <Alert/>
-                        <Switch>
-                            <Route exact path='/register' component={Register}/>
-                            <Route exact path='/login' component={Login}/>
-                            <Route exact path='/profiles' component={Profiles}/>
-                            <Route exact path='/profile/:id' component={Profile}/>
-                            <PrivateRoute exact path='/dashboard' component={Dashboard}/>
-                            <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
-                            <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
-                            <PrivateRoute exact path='/add-experience' component={AddExperience}/>
-                            <PrivateRoute exact path='/add-education' component={AddEducation}/>
-                            <PrivateRoute exact path='/posts' component={Posts}/>
-                            <PrivateRoute exact path='/posts/:id' component={Post}/>
-                        </Switch>
-                    </section>
-                </Fragment>
-            </Router>
-        </Provider>
-    )
-}
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
